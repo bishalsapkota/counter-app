18572eac8854a1ac7be8bf0380434ca31b0e2f42
WP Capsules <wpcapsules@WPs-MacBook-Pro.local>
2019-03-29 14:46:30 +0545

    Updates

---
+++ precache-manifest.0eb36bf464eade93d5b23a4c616b80fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "e6c321481940f43776bf",
+    "url": "/counter-app/static/js/main.c78b0245.chunk.js"
+  },
+  {
+    "revision": "9c77122eb7aea341cb90",
+    "url": "/counter-app/static/js/2.6fde730c.chunk.js"
+  },
+  {
+    "revision": "e6c321481940f43776bf",
+    "url": "/counter-app/static/css/main.e50255af.chunk.css"
+  },
+  {
+    "revision": "9c77122eb7aea341cb90",
+    "url": "/counter-app/static/css/2.3c4b89e0.chunk.css"
+  },
+  {
+    "revision": "5dc8f58cc90084ca1756c877f21362df",
+    "url": "/counter-app/index.html"
+  }
+];
